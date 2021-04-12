bb54828ee90aa256d8cf3e5c76e02657108b9e64
Celina Rabe <celinarabe@yahoo.com>
2021-04-12 12:50:20 -0500

    commented out h1

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   return (
     <div>
       {/* not working */}
-      <h1>Campaigns: {campaigns[0].title}</h1>
+      {/* <h1>Campaigns: {campaigns[0].title}</h1> */}
     </div>
   );
 }
