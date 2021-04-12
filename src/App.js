27814d17754659dfe9e09df4ffb4f28b7999643d
Celina Rabe <celinarabe@yahoo.com>
2021-04-12 12:03:27 -0500

    component did update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import { FirebaseApp, FirebaseDb } from "./firebase";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
+//TO DO: remove this when we get to production
 const firebaseApp = new FirebaseApp().app; //creating new firebase app object and pulling the app property from it
 const db = new FirebaseDb(firebaseApp);
-console.log(db.getCampaigns());
 
 function App() {
-  const [campaigns, setCampaigns] = useState(db.getCampaigns());
+  const [campaigns, setCampaigns] = useState([]);
+  const didMountCampaigns = useRef(false);
 
+
+  //fetching campaigns
   useEffect(() => {
-    console.log(campaigns);
-    console.log(campaigns[0]) //not working
-  },[]);
+    //defining async function
+    const fetchCampaigns = async () => {
+      const campaigns = await db.getCampaigns();
+     setCampaigns(campaigns);
+       console.log('in fetch: ',campaigns)
+      didMountCampaigns.current = true;
+    };
+    fetchCampaigns();
+  }, []);
+
+  console.log('after use effect')
+
+  //mimics component did update
+  useEffect(() => {
+    if (didMountCampaigns.current) {
+      console.log(campaigns[0].title);
+    }
+  });
 
   return (
     <div>
       <h1>Campaigns:</h1>
-      <p>{campaigns[0]}</p>
-
+      <p></p>
     </div>
   );
 }
--- src/Firebase/firebaseDb.js
+++ src/Firebase/firebaseDb.js
@@ -5,22 +5,31 @@ class FirebaseDb {
     this.db = app.firestore(); //getting db from the app object we pass in
   }
 
-  getCampaigns() {
-    var campaignArr = [];
-    this.db
+  //async function returns a promise
+  getCampaigns = async () => {
+    const querySnapshot = await this.db
       .collection("campaign")
       .withConverter(campaignConverter)
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          var campaign = doc.data();
-          campaignArr.push(campaign);
-        });
-        
-      });
-      return campaignArr;
-  }
+      .get(); //wait until this is done to move onto next line
+    return querySnapshot.docs.map((doc) => doc.data());
+  };
+
+  // getCampaigns() {
+  //   var campaignArr = [];
+  //   this.db
+  //     .collection("campaign")
+  //     .withConverter(campaignConverter)
+  //     .get()
+  //     .then((querySnapshot) => {
+  //       querySnapshot.forEach((doc) => {
+  //         // doc.data() is never undefined for query doc snapshots
+  //         var campaign = doc.data();
+  //         campaignArr.push(campaign);
+  //       });
+
+  //     });
+  //     return campaignArr;
+  // }
 }
 
 export default FirebaseDb;
