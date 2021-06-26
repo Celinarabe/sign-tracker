cb0a9a91d6ff9c67620f4e4e090684d0868849fd
Celina Rabe <celinarabe@yahoo.com>
2021-06-25 19:46:48 -0500

    added preview photo

--- src/images/index.js
+++ src/images/index.js
@@ -4,10 +4,12 @@ import DeviceImg from './device.png'
 import IphoneImg from './mac:iphone.png'
 import HowToVideo from './how-it-works.mp4'
 
+
 export {
   MapBg,
   Sign1,
   DeviceImg,
   IphoneImg,
   HowToVideo,
+
 };
